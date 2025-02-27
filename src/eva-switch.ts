7e479457482ca9f404d06f3c3c4b1dddef7a9fb1
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-02-27 09:08:58 -0500

    Clean up code for consistency

--- src/eva-switch.ts
+++ src/eva-switch.ts
@@ -1,7 +1,7 @@
 type EVA_SWITCH_DEFAULT_CALLBACK = (value: any) => void;
 type EVA_SWITCH_DEFAULT = (callback: EVA_SWITCH_DEFAULT_CALLBACK) => void;
 
-type EVA_SWITCH_CASE_EVALUATE = ((value: any) => any);
+type EVA_SWITCH_CASE_EVALUATE = (value: any) => any;
 type EVA_SWITCH_CASE_RETURN = { default: EVA_SWITCH_DEFAULT, case: EVA_SWITCH_CASE, do: EVA_SWITCH_DO };
 type EVA_SWITCH_CASE = (evaluate: RegExp | EVA_SWITCH_CASE_EVALUATE, caseId: any) => EVA_SWITCH_CASE_RETURN;
 
