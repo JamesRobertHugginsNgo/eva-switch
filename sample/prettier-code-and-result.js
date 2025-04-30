dd963ac59573b72bf8bd8c5e8258e5eb70f6fb61
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-04-30 16:23:38 -0400

    Replace node_module with jscdn

--- sample/prettier-code-and-result.js
+++ sample/prettier-code-and-result.js
@@ -1,6 +1,10 @@
-import Prettier from '../node_modules/prettier/standalone.mjs';
-import prettierBabelPlugin from '../node_modules/prettier/plugins/babel.mjs';
-import prettierEstreePlugin from '../node_modules/prettier/plugins/estree.mjs';
+// import Prettier from '../node_modules/prettier/standalone.mjs';
+// import prettierBabelPlugin from '../node_modules/prettier/plugins/babel.mjs';
+// import prettierEstreePlugin from '../node_modules/prettier/plugins/estree.mjs';
+
+import Prettier from 'https://cdn.jsdelivr.net/npm/prettier@3.5.3/standalone.min.mjs';
+import prettierBabelPlugin from 'https://cdn.jsdelivr.net/npm/prettier@3.5.3/plugins/babel.min.mjs';
+import prettierEstreePlugin from 'https://cdn.jsdelivr.net/npm/prettier@3.5.3/plugins/estree.min.mjs';
 
 const prettierJsOptions = {
 	parser: 'babel',
